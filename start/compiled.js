ab9c004a0be8bfead5f16dd0e57769645e2f9774
Abrahan Silverio <abrahansmg@gmail.com>
2017-08-21 10:55:53 -0400

    changed to es6 arrow functions

--- start/compiled.js
+++ start/compiled.js
@@ -2,37 +2,37 @@
 
 $(function () {
   var $document = $(document);
-  var preloadAssets = [// add any assets to be preloaded to this array
+  var preloadAssets = [
+  // add any assets to be preloaded to this array
   '//cdnjs.cloudflare.com/ajax/libs/gsap/1.20.2/TweenMax.min.js', '/sparkflow/formats/latest/adchoices.min.js', '/sparkflow/formats/latest/utmark.min.js'];
-
-  var name = 'sx';
-  console.log(name + 'ya');
 
   ad.preload(preloadAssets);
 
   $document.on('adReady', function () {
     mraid.cancelAutoClose();
     insertAdBranding();
-    ssInit($document);
+    startAd($document);
   });
 });
 
-function ssInit($document) {}
+var startAd = function startAd($document) {};
 
 // FUNCTIONS
-function setCtaAction($el) {
+var setCtaAction = function setCtaAction($el) {
   $el.attr('id').indexOf('BACKGROUND_') !== -1 ? TweenMax.set($el, { display: 'block', width: '200%', height: '200%' }) : TweenMax.set($el, { display: 'block' });
-}
+};
 
-function insertAdBranding() {
-  AdChoices.init({ // AdChoices
+var insertAdBranding = function insertAdBranding() {
+  // AdChoices
+  AdChoices.init({
     corner: 'br', // corner property which where the icon will be anchored: tr, tl, br or bl
     url: 'http://www.undertone.com/opt-out-tool?utm_source=AdChoiceIcon&utm_medium=IAAdChoicesIcon&utm_campaign=Privacy'
   });
 
-  UndertoneMark.init({ // Undertone
+  // Undertone
+  UndertoneMark.init({
     corner: 'bl', // corner property which where the icon will be anchored: tr, tl, br or bl
     color: 'black',
     opacity: 0.5
   });
-}
+};
--- start/js.es6
+++ start/js.es6
@@ -1,44 +1,39 @@
-$(function () {
-  let $document = $(document)
-  let preloadAssets = [
+$(() => {
+  const $document = $(document)
+  const preloadAssets = [
     // add any assets to be preloaded to this array
     '//cdnjs.cloudflare.com/ajax/libs/gsap/1.20.2/TweenMax.min.js',
     '/sparkflow/formats/latest/adchoices.min.js',
     '/sparkflow/formats/latest/utmark.min.js'
   ]
 
-  let sta = setCtaAction($document)
-
-  sta()
-
   ad.preload(preloadAssets)
 
-  $document.on('adReady', function () {
+  $document.on('adReady', () => {
     mraid.cancelAutoClose()
     insertAdBranding()
-    ssInit($document)
-    console.log('peero')
+    startAd($document)
   })
 })
 
-function ssInit ($document) {}
+const startAd = $document => {}
 
 // FUNCTIONS
-function setCtaAction ($el) {
+const setCtaAction = $el => {
   $el.attr('id').indexOf('BACKGROUND_') !== -1
     ? TweenMax.set($el, { display: 'block', width: '200%', height: '200%' })
     : TweenMax.set($el, { display: 'block' })
 }
 
-function insertAdBranding () {
+const insertAdBranding = () => {
+  // AdChoices
   AdChoices.init({
-    // AdChoices
     corner: 'br', // corner property which where the icon will be anchored: tr, tl, br or bl
     url: 'http://www.undertone.com/opt-out-tool?utm_source=AdChoiceIcon&utm_medium=IAAdChoicesIcon&utm_campaign=Privacy'
   })
 
+  // Undertone
   UndertoneMark.init({
-    // Undertone
     corner: 'bl', // corner property which where the icon will be anchored: tr, tl, br or bl
     color: 'black',
     opacity: 0.5
